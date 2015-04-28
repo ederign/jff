d5234e63c50ed4a1fbd92dc858b07f6e015cc969
ederign <ignatowicz@gmail.com>
2015-04-27 22:24:30 -0300

    First hacker rank ieiii

---
+++ hackerrank/src/main/java/me/ederign/SolveMeFirst.java
@@ -0,0 +1,14 @@
+package me.ederign;
+
+import java.util.Scanner;
+
+public class SolveMeFirst {
+
+    public static void main( String[] args ) {
+        Scanner scanner = new Scanner(System.in);
+        int first = scanner.nextInt();
+        int second = scanner.nextInt();
+        int result = first + second;
+        System.out.print( result );
+    }
+}
